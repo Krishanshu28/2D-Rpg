1bee569768f79eac9449ef73d0ebb97f2f886936
Krishanshu Srivastava <krishnasrivastava2811@gmail.com>
2024-02-07 00:28:16 +0530

    Character Movement

---
+++ TopDown2D/Assets/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Animator animator;
+   public float Health
+    {
+        set
+        {
+            health = value;
+
+            if(health <= 0)
+            {
+                Defeated();
+            }
+        }
+        get { return health; }
+    }
+
+    public float health = 1f;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Defeated()
+    {
+        animator.SetTrigger("Defeated");
+    }
+
+    public void RemoveEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
